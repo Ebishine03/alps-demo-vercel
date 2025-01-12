ba64c22f2cdaae64cdfd9cf0444fc082f86fce2a
Ebi Shine <ebishine03@gmail.com>
2025-01-12 13:59:14 +0530

    Setting changed for demo host

--- Alps/settings.py
+++ Alps/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9d#@4%3ho7vyvcjcrq4ljf_qqczi0e4uddj4*^$lvzw&%2qj+u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
